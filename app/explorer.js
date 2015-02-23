96fa048294a73bd69a2a434e4fd6fb5d5c1dbb5c
Michael Allen <michael@michaelallen.io>
2015-02-23 12:17:37 +0000

    enact demo to show dating a charge

--- app/explorer.js
+++ app/explorer.js
@@ -2,17 +2,20 @@
 var explorer = module.exports = {
   renderPage : function (req, res) {
     var path = (req.params[0]);
-    
+
     res.render(path, function(err, html) {
-    	if (err) {
-    		console.log(err);
-    		res.send(404);
-    	} else {
-    		res.end(html);
-    	}
+      if (err) {
+        console.log(err);
+        res.send(404);
+      } else {
+        res.end(html);
+      }
     });
   },
   index : function (req, res) {
     res.render("index");
+  },
+  enactDemo : function (req, res) {
+    res.render("enact/index", {layout: 'layouts/enact'});
   }
 }
